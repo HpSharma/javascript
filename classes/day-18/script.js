db8a6520c9e4977ffe1b43d0d64d99ecfc0efaf8
HpSharma <harpalhmh@gmail.com>
2025-04-10 22:40:42 -0400

    web storage

---
+++ classes/day-18/script.js
@@ -0,0 +1,42 @@
+// web storage
+// 1. local Storage 2. session Storage 3. cookies 4. indexDB
+
+// -------------- Local Storage --------------
+
+// localStorage.setItem('theme', 'dark');
+// const theme = localStorage.getItem('theme');
+// // console.log(theme);
+// console.log(localStorage.length);
+// // console.log(localStorage.key(0))
+// // console.log(localStorage.getItem(localStorage.key(0)));
+//
+// localStorage.setItem('username', 'sanjay');
+// // console.log(localStorage.key(1));
+//
+// localStorage.setItem('mail-domain', 'gmail.com');
+// // console.log(localStorage.key(2));
+//
+// // localStorage.removeItem('username');
+//
+// const username = {
+//     name: 'Sanjay',
+//     age: 23,
+//     gender: 'male'
+// }
+// // console.log(username.toString());
+// localStorage.setItem('user', JSON.stringify(username));
+//
+// let sanjay = localStorage.getItem('user');
+// console.log(sanjay);
+// sanjay = JSON.parse(sanjay);
+// console.log(sanjay);
+// console.log(sanjay.age);
+
+// ---------- Session Storage ------------
+
+// sessionStorage.setItem('username', 'sanjay');
+// sessionStorage.setItem('password', '23423423');
+//
+// const username = sessionStorage.getItem('username');
+// console.log(username);
+
